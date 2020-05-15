424bac976416c344e1b73e60087aebc97d8ceb56
Erica Gutierrez <egutierrez411@gmail.com>
2020-05-15 10:47:59 -0400

    first commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Erica on 5/15/20.
+//  Copyright © 2020 Perseidas LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
