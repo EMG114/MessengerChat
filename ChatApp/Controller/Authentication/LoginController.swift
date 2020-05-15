d51e9abc76d35a05808cbdc3b16422d20e48cefc
Erica Gutierrez <egutierrez411@gmail.com>
2020-05-15 17:15:08 -0400

    setupp LoginVC completed

--- ChatApp/Controller/Authentication/LoginController.swift
+++ ChatApp/Controller/Authentication/LoginController.swift
@@ -32,8 +32,9 @@ return InputContainerView(image: UIImage(systemName: "lock"), textField: passwor
         let button = UIButton(type: .system)
         button.setTitle("Log In", for: .normal)
         button.layer.cornerRadius = 5
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        button.backgroundColor = .red
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
         button.setHeight(height: 50)
         return button
     }()
@@ -46,6 +47,15 @@ return InputContainerView(image: UIImage(systemName: "lock"), textField: passwor
         return tf
     }()
     
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributeTitle = NSMutableAttributedString(string:"Don't have an account? ", attributes: [.font:UIFont.systemFont(ofSize: 16), .foregroundColor: UIColor.white])
+        attributeTitle.append(NSMutableAttributedString(string:"Sign Up", attributes: [.font:UIFont.boldSystemFont(ofSize: 16), .foregroundColor: UIColor.white]))
+        button.setAttributedTitle(attributeTitle, for:.normal)
+        button.addTarget(self, action: #selector(handleShowSignUp), for:.touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     
@@ -55,6 +65,12 @@ return InputContainerView(image: UIImage(systemName: "lock"), textField: passwor
         
     }
     
+    // MARK: - Selectors
+    
+    @objc func handleShowSignUp() {
+       let controller = RegistrationController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
     
     // MARK: - Helpers
     
@@ -75,6 +91,9 @@ return InputContainerView(image: UIImage(systemName: "lock"), textField: passwor
         
         view.addSubview(stack)
         stack.anchor(top:iconImage.bottomAnchor, left:view.leftAnchor, right: view.rightAnchor, paddingTop:32, paddingLeft:32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left:view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 32, paddingBottom: 16,  paddingRight: 32)
     }
     
     func configureGradientLayer() {
