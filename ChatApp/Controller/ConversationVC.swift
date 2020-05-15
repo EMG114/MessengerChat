3a279907267d4f096da9d7331b8637dbd0ebdcaf
Erica Gutierrez <egutierrez411@gmail.com>
2020-05-15 11:21:21 -0400

    setup navigation bar

---
+++ ChatApp/Controller/ConversationVC.swift
@@ -0,0 +1,62 @@
+//
+//  ConversationVC.swift
+//  ChatApp
+//
+//  Created by Erica on 5/15/20.
+//  Copyright © 2020 Perseidas LLC. All rights reserved.
+//
+
+import UIKit
+
+
+class ConversationVC: UIViewController {
+    
+    
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        configureNavigationBar()
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+    
+    // MARK - Selectors
+    
+    @objc func showProfile() {
+        
+    }
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemPurple
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
+        
+    }
+    
+}
